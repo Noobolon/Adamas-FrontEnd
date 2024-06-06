eaf69686882d5ba9249c62bb61891c71e33b5989
noobolon <noobolons@gmail.com>
2024-06-05 23:00:43 -0300

    Começo da Toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+import ToolBar from './components/structure/ToolBar.vue'
 
 </script>
 
 <template>
+  <ToolBar />
+  
   <h1>aaaaaaaaaaaaaaaaaaaaaa</h1>
 
 </template>
---
+++ src/components/structure/ToolBar.vue
@@ -0,0 +1,64 @@
+<script>
+    export default{
+        name: "ToolBar"  
+    }
+
+</script>
+
+
+<template>
+    <header>
+        <ul>
+            <li>
+                <img src="/logos/AdamasWhite.png" alt="Logo">
+            </li>
+             <li><a href="https://www.youtube.com">Projetos</a></li>
+            <li><a href="https://www.facebook.com">Eventos</a></li>
+            <li><a href="https://www.instagram.com">Contatos</a></li>
+        </ul>
+
+        <div>
+            <p>teste</p>
+        </div>
+        
+    </header>
+</template>
+
+
+<style scoped>
+
+header{
+    padding: 2%;
+
+    background-color: var(--ToolbarColor);
+    color: var(--Text2);
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+a{
+    font-size: 18pt;
+
+    text-decoration: none;
+    text-align: center;
+    color: var(--Text2);
+}
+a::after{
+    color: var(--Text2);
+}
+
+ul{
+    list-style: none;
+    display: flex;
+    justify-content: space-evenly;
+}
+
+li{
+    margin: 0px 10%;
+    border-left: 2px;
+}
+
+
+</style>
