779ce2104945d826c30890641af326ceb82bd50d
Etec Antonio Devisate <etec@devisate.com.br>
2024-08-20 08:52:57 -0300

    testando axios (incompleto)

--- src/router/index.js
+++ src/router/index.js
@@ -63,8 +63,3 @@ const router = createRouter({
 export default router
 
 
-
-// route level code-splitting
-// this generates a separate chunk (About.[hash].js) for this route
-// which is lazy-loaded when the route is visited.
-  // component: () => import('../components/pages/CreateAccount.vue')
--- src/views/register/RegUser.vue
+++ src/views/register/RegUser.vue
@@ -1,19 +1,59 @@
 <script>
+import api from '@/api';
+import axios from 'axios';
+
 export default {
-    name: "RegUser"
+    name: "RegUser",
+
+    data() {
+        return{
+            name: this.name,
+            nickname: this.nickname,
+            email: this.email,
+            password: this.password,
+        }   
+    },
+
+    methods: {
+        async regUser(){
+
+            axios.post('localhost:3000/create', {
+                name: this.name,
+                nickname: this.nickname,
+                email: this.email,
+                password: this.password
+            })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {                    
+                console.log(error);
+            });
+
+            //debug
+            console.log(this.name, this.email, this.password)
+        }
+    }
+
 }
 </script>
 
 <template>
     <main>
         <h1>Cadastro de Usuário</h1>
+
+        <!-- debug -->
+        <p>teste: <br>nome: {{ name }} email: {{ email }} senha: {{ password }}</p>
+
         <div>
             <img src="../../assets/images/PencilDiamond.png" alt="Diamante" >
-            <form action="">
-                <input type="text" placeholder="Nome" name="name" id="name">
-                <input type="email" placeholder="E-mail" name="email" id="email">
-                <input type="password" placeholder="Senha" name="senha" id="senha">
-                <input type="button" value="Cadastrar">
+            <form @submit.prevent="cadastrar">
+                <input v-model="name" placeholder="Nome" id="name">
+                <input v-model="nickname" placeholder="Apelido (opcional)" id="nickname">
+                <input v-model="email" placeholder="E-mail" id="email">
+                <input v-model="password" placeholder="Senha" id="password">
+
+                <input type="button" @click="regUser()" value="Cadastrar">
                 <p>Já possui uma conta?<br><RouterLink to="/login/usuario">Entrar</RouterLink></p>
             </form>
         </div>
