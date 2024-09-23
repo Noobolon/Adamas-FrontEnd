b9e7c65624247198ce3d3d22b5619eb59be33818
Etec Antonio Devisate <etec@devisate.com.br>
2024-09-23 12:55:00 -0300

    correção do CSS da tela de pesquisa

--- src/views/search/Projects.vue
+++ src/views/search/Projects.vue
@@ -9,7 +9,7 @@ export default{
         return {
             search_content: this.search_content,
             // projects_list: this.projects_list
-            projeto_exemplo: [{"name": "adamas", "owner": "aluno", "desc": "descrição boa"}]
+            projeto_exemplo: {"name": "adamas", "owner": "aluno", "desc": "descrição boa"}
         };
     },
     methods: {
@@ -26,33 +26,36 @@ export default{
 </script>
 
 <template>
-    <aside class="tag_container">
-        <p>aaa</p>
-        <p>aaa</p>
-        <p>aaa</p>
-    </aside>   
-
     <main>
+        <div class="content">
+            <div class="srch_container">
+                <input type="text" placeholder="Pesquisar projetos..." v-model="search_content">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </div>
+            <nav class="ctgry_container">
+                <RouterLink to="/projetos" id="selected">Projetos</RouterLink>
+                <RouterLink to="/eventos">Eventos</RouterLink>
+                <RouterLink to="/usuarios">Usuários</RouterLink>
+            </nav>
+
+            <div class="cntnt_container">
+                <!-- <ul>
+                    <li v-for="projeto in projects_list">
+                        <ProjectComponent :project="projeto"/>
+                    </li>
+                </ul> -->
         
-        <div class="srch_container">
-            <input type="text" placeholder="Pesquisar projetos..." v-model="search_content">
-            <button type="submit"><i class="fa fa-search"></i></button>
+                <ProjectComponent :project="projeto_exemplo" />
+            </div>
+
         </div>
 
-        <nav class="contnt_container">
-            <RouterLink to="/projetos" id="selected">Projetos</RouterLink>
-            <RouterLink to="/eventos">Eventos</RouterLink>
-            <RouterLink to="/usuarios">Usuários</RouterLink>
-        </nav>
-        
-        <!-- <ul>
-            <li v-for="projeto in projects_list">
-                <ProjectComponent :project="projeto"/>
-            </li>
-        </ul> -->
-        
-        <ProjectComponent :project="projeto_exemplo" />
-          
+        <aside class="tag_container">
+            <p>aaa</p>
+            <p>aaa</p>
+            <p>aaa</p>
+        </aside>   
+
     </main>
     
 </template>
